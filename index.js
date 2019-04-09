3f1cb154d6f70323499fa0bdfbf82fb33ddb2a7f
brellin <cekakid@gmail.com>
2019-04-08 19:03:12 -0500

    added []

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export const useCDM = (url, setData) => {
                 .then(res => setData(res.data))
                 .catch(err => console.log(err))
         setMounted(true)
-    })
+    }, [])
 
     return {
         mounted,
