432c00c843baf5ed75501b3dc94ebc483b6f8103
brellin <cekakid@gmail.com>
2019-06-02 23:16:14 -0500

    remove useCDM and useCDU

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-export const useCDM = (url, setData) => {
-    useEffect(() => {
-        axios
-            .get(url)
-            .then(res => setData(res.data))
-            .catch(err => console.log(err))
-        setMounted(true)
-    }, [])
-}
-
-export const useCDU = (url, setData) => {
-    const [updated, setUpdated] = useState(false);
-
-    useEffect(() => {
-        axios
-            .get(url)
-            .then(res => setData(res.data))
-            .catch(err => console.log(err))
-        setUpdated(true)
-    }, [updated])
-
-    return {
-        updated,
-        setUpdated
-    }
-}
+import { useState } from 'react';
 
 export const useForm = cb => {
     const [fields, setFields] = useState({});
@@ -36,7 +8,7 @@ export const useForm = cb => {
         cb();
     }
 
-    const handleChanges = (e) => {
+    const handleChanges = e => {
         e.persist();
         setFields({ ...fields, [e.target.name]: e.target.value });
         localStorage.setItem(`${e.target.name}`, e.target.value);
