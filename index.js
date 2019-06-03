51e86091f3e33677d306c3a5b49f8d5d05c1d546
brellin <cekakid@gmail.com>
2019-06-02 23:18:46 -0500

    fix useForm

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
 import { useState } from 'react';
-=======
-import { useState, useEffect } from 'react';
-
-export const useCDM = (func) => {
-    useEffect(() => {
-        func
-    }, [])
-}
-
-export const useCDU = (func) => {
-    const [updated, setUpdated] = useState(false);
-
-    useEffect(() => {
-        func
-        setUpdated(true)
-    }, [updated])
-
-    return {
-        updated,
-        setUpdated
-    }
-}
->>>>>>> 88b5ccc37448bc849ea05199b905e1ce4ed9f7fd
 
 export const useForm = cb => {
     const [fields, setFields] = useState({});
@@ -33,9 +9,8 @@ export const useForm = cb => {
     }
 
     const handleChanges = e => {
-        e.persist();
+        e.preventDefault();
         setFields({ ...fields, [e.target.name]: e.target.value });
-        localStorage.setItem(`${e.target.name}`, e.target.value);
     }
 
     return {
